20a4afc050d3293a460c6adbc60a87335906491b
Walabed <110702739+wasan81@users.noreply.github.com>
2022-08-19 10:21:31 -0400

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,186 @@
+/*
+Wasan Alabed
+CS210
+Project 3 coner grocer
+*/
+#include <Python.h>
+#include <iostream>
+#include <Windows.h>
+#include <cmath>
+#include <string>
+#include <fstream>
+using namespace std;
+
+/*
+Description:
+	To call this function, simply pass the function name in Python that you wish to call.
+Example:
+	callProcedure("printsomething");
+Output:
+	Python will print on the screen: Hello from python!
+Return:
+	None
+*/
+void CallProcedure(string pName)
+{
+	char* procname = new char[pName.length() + 1];
+	std::strcpy(procname, pName.c_str());
+
+	Py_Initialize();
+	PyObject* my_module = PyImport_ImportModule("pythonCode");
+	PyErr_Print();
+	PyObject* my_function = PyObject_GetAttrString(my_module, procname);
+	PyObject* my_result = PyObject_CallObject(my_function, NULL);
+	Py_Finalize();
+
+	delete[] procname;
+}
+
+/*
+Description:
+	To call this function, pass the name of the Python functino you wish to call and the string parameter you want to send
+Example:
+	int x = callIntFunc("PrintMe","Test");
+Output:
+	Python will print on the screen:
+		You sent me: Test
+Return:
+	100 is returned to the C++
+*/
+int callIntFunc(string proc, string param)
+{
+	char* procname = new char[proc.length() + 1];
+	std::strcpy(procname, proc.c_str());
+
+	char* paramval = new char[param.length() + 1];
+	std::strcpy(paramval, param.c_str());
+
+
+	PyObject* pName, * pModule, * pDict, * pFunc, * pValue = nullptr, * presult = nullptr;
+	// Initialize the Python Interpreter
+	Py_Initialize();
+	// Build the name object
+	pName = PyUnicode_FromString((char*)"pythonCode");
+	// Load the module object
+	pModule = PyImport_Import(pName);
+	// pDict is a borrowed reference 
+	pDict = PyModule_GetDict(pModule);
+	// pFunc is also a borrowed reference 
+	pFunc = PyDict_GetItemString(pDict, procname);
+	if (PyCallable_Check(pFunc))
+	{
+		pValue = Py_BuildValue("(z)", paramval);
+		PyErr_Print();
+		presult = PyObject_CallObject(pFunc, pValue);
+		PyErr_Print();
+	}
+	else
+	{
+		PyErr_Print();
+	}
+	//printf("Result is %d\n", _PyLong_AsInt(presult));
+	Py_DECREF(pValue);
+	// Clean up
+	Py_DECREF(pModule);
+	Py_DECREF(pName);
+	// Finish the Python Interpreter
+	Py_Finalize();
+
+	// clean 
+	delete[] procname;
+	delete[] paramval;
+
+
+	return _PyLong_AsInt(presult);
+}
+
+/*
+Description:
+	To call this function, pass the name of the Python functino you wish to call and the string parameter you want to send
+Example:
+	int x = callIntFunc("doublevalue",5);
+Return:
+	25 is returned to the C++
+*/
+int callIntFunc(string proc, int param)
+{
+	char* procname = new char[proc.length() + 1];
+	std::strcpy(procname, proc.c_str());
+
+	PyObject* pName, * pModule, * pDict, * pFunc, * pValue = nullptr, * presult = nullptr;
+	// Initialize the Python Interpreter
+	Py_Initialize();
+	// Build the name object
+	pName = PyUnicode_FromString((char*)"pythonCode");
+	// Load the module object
+	pModule = PyImport_Import(pName);
+	// pDict is a borrowed reference 
+	pDict = PyModule_GetDict(pModule);
+	// pFunc is also a borrowed reference 
+	pFunc = PyDict_GetItemString(pDict, procname);
+	if (PyCallable_Check(pFunc))
+	{
+		pValue = Py_BuildValue("(i)", param);
+		PyErr_Print();
+		presult = PyObject_CallObject(pFunc, pValue);
+		PyErr_Print();
+	}
+	else
+	{
+		PyErr_Print();
+	}
+	//printf("Result is %d\n", _PyLong_AsInt(presult));
+	Py_DECREF(pValue);
+	// Clean up
+	Py_DECREF(pModule);
+	Py_DECREF(pName);
+	// Finish the Python Interpreter
+	Py_Finalize();
+
+	// clean 
+	delete[] procname;
+
+	return _PyLong_AsInt(presult);
+}
+
+void main()
+{
+
+	cout << "TOTAL INVENTORY" << endl;
+	CallProcedure("determineInventory"); //print item anf frequency
+
+	cout << endl;
+
+	string userString; // user input
+	cout << "What item do you want frequency for? " << endl;
+	cin >> userString; //Grabs product the user wishes to look up
+	
+
+	
+	cout << userString << " occur " << callIntFunc("determineFrequency", userString) << endl; // print single item
+
+	
+	
+	cout << "WRITE OUTPUT TO frequency.dat" << endl; //write itime and frequency in frequency.dat file
+	cout << endl;
+	CallProcedure("output");
+	//After the python execution
+	//Read the frequency.dat file, and create the histogram using C++
+	ifstream myFile;
+	myFile.open("frequency.dat");
+	if (myFile.is_open()) {
+		string item;
+		int frequency;
+		while (myFile >> item >> frequency) {
+			cout << item;
+			for (int i = 0; i < frequency; i++) {
+
+				cout << "*";
+			}
+			cout << endl;
+
+		}
+		myFile.close();
+
+	}
+}
---
+++ pythonCode.py
@@ -0,0 +1,69 @@
+FILENAME = open("item.txt")
+
+def readFile(filename):
+    
+   # Get the list of lines read in from the file in raw form.
+   
+    lines = None
+    
+    with open("item.txt", "r") as file:
+        lines = file.readlines()
+     
+    return lines   #return - the list of lines
+
+
+def getInventory():
+    '''
+    Get the inventory read in from the file.
+    @return - dictionary of inventory in the form {item : frequency}
+    '''
+   # Dictionary of {item : frequency} 
+    inventory = {}
+    #read item from the item.txt file
+    items =readFile(FILENAME)
+   # Loop through the list of items
+    
+    for item in items:
+        
+        # Remove the newline character at end of line. 
+        item = item.strip()
+   # Update existing frequency; otherwise add item to dictionary
+        if item in inventory:
+            inventory[item] +=1 # accumulate to the dictionary item
+        else:
+            inventory[item] =1
+            
+    return inventory
+
+    
+def determineInventory():
+    # Get the inventory
+   inventory = getInventory()
+   
+    # loop through the dictionary's keys and values
+   
+   for item, frequency in inventory.items():
+       print(item,":", frequency) # print the output
+
+def determineFrequency(item):
+    # Get the inventory
+    inventory = getInventory()
+   
+    # Access the item from the inventory
+    
+    return inventory[item] # return single item
+
+def output():
+    #get the inventory
+    inventory = getInventory()
+   
+    # Loop through the dictionary and write to the loop through the dictionary's keys and valuest file
+   
+
+    myFile = open("frequency.dat", "w")
+   
+    for item, frequency in inventory.items():
+    	myFile.write(item+" "+str(frequency)+"\n")
+    
+    
+    
